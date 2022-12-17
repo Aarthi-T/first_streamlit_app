97a1cb91e879d5c178b17232ae3054c1387bb840
AARTHI THINAKARAN <60143903+Aarthi-T@users.noreply.github.com>
2022-12-17 12:38:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ except URLError as e:
 streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
-def fruit_load_list():
+def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("use warehouse pc_rivery_wh")
     my_cur.execute("select * from fruit_load_list")
